b8e8584bab5dd7a47546bb630d766630e81976a4
Lisa Gee <zhusufeng@gmail.com>
2023-08-26 17:05:00 -0700

    Update transformCSVToTransactionArray & create createTransaction

--- helpers.js
+++ helpers.js
@@ -1,18 +1,33 @@
-function transformCSVToTransactionArray(csvString) {
+const GoodPOSTransaction = require("./sources/GoodPOS");
+const OkPOSTransaction = require("./sources/OkPOS");
+
+function createTransaction(source) {
+  let transaction;
+  switch (source) {
+    case "good_pos":
+      transaction = new GoodPOSTransaction();
+      break;
+    case "ok_pos":
+      transaction = new OkPOSTransaction();
+      break;
+    // case "wonky_pos":
+    //   break;
+  }
+  return transaction;
+}
+
+function transformCSVToTransactionArray(csvString, source) {
   const parsed = csvString.split("\n");
-  const [firstRow, ...rows] = parsed;
-  const headers = firstRow.split(",");
-  const transactions = rows
-    .filter(row => row.length)
-    .map(row => {
-      const cells = row.split(",");
-      const rowObj = headers.reduce((acc, header, idx) => {
-        return cells.length ? { ...acc, [header]: cells[idx] } : acc;
-      }, {});
-      return rowObj;
-    });
+  const [_firstRow, ...rows] = parsed;
+  const trimmedRows = rows.filter(row => row.length);
+  const transactions = trimmedRows.map(row => {
+    const transaction = createTransaction(source);
+    transaction.set(row);
+    return transaction;
+  });
   return transactions;
 }
+
 function createInsertStatement(transaction) {
   const { id, budtenderId, locationId, basketSize, timestamp } = transaction;
   const insertStmt = `
